c817368bac418f06841b4c269bb487ac53d88e13
Daniel Cruz <danielcruzp@unitec.edu>
2025-11-22 17:47:04 -0600

    Inicializacion de frontend

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-red-500 text-8xl font-extrabold text-center leading-tight">
+        Frontend de proyecto de analisis de algoritmos
+      </div>
+    </div>
+  );
+}
+
+export default App;
