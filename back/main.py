36cec8a40df2f22ac2aca75ac0c0a4f1bb78bbfa
Daniel Cruz <danielcruzp@unitec.edu>
2025-12-04 00:22:30 -0600

    added cors middleware to backend

--- back/main.py
+++ back/main.py
@@ -3,8 +3,17 @@
 
 from fastapi import FastAPI
 from algoritmos.knapsack import knapsack_exact, knapsack_greedy
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def hello_world():
